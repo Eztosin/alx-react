77abd4fed2ed7424f2bff0445555e1f7169c5d4d
Eztosin <tosinezekiel03@gmail.com>
2024-04-02 14:14:09 +0000

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+//Adding the paragraphs
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
