6aaa88dff399a6d0287ebca5b6d2a2bd871d0b53
Eztosin <tosinezekiel03@gmail.com>
2024-04-02 14:23:48 +0000

    Add git-ignore

--- task_0/src/index.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-
-//Adding the paragraphs
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
