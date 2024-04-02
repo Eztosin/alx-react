11f96952b419fbc3b135d5c1ebd9dc5d744e8cfa
Eztosin <tosinezekiel03@gmail.com>
2024-04-02 14:07:27 +0000

    Basic setup

--- task_0/src/index.js
+++ task_0/src/index.js
@@ -1,1 +1,6 @@
-sghahrrguihsfkbdb
+import $ from 'jquery';
+
+//Adding the paragraphs
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
