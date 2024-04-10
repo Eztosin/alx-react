221e1ca19a75844316e044f99509df385721d8e6
Eztosin <tosinezekiel03@gmail.com>
2024-04-10 12:06:42 +0000

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import './Notifications.css'
+import './Notifications.css';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
+
+
 
 export default function Notification() {
     return (
 	    <div className="Notifications">
-	    <p>Here is the list of notifications</p>
-	    </div>
-    );
-}
+	    <button
+	style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", fontSize: "15px", position: "absolute", aria-label="Close"
+		 onClick={console.log("Close button has been clicked")}
+		 >
+		 <img src={closeIcon} alt="closeIcon" width="10px" />
+		 </button>
+		 <p>Here is the list of notifications</p>
+		 <ul>
+		 <li data="default">New course available</li>
+		 <li data="urgent">New resume available</li>
+		 <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+		 </ul>
+		 </div>
+	       );
+	      }
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,7 @@ export default function getFullYear() {
 export default funtion getFooterCopy(isIndex) {
     return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 }
+
+export default function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+}
