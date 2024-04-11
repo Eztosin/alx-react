6b9ceb113cc0e9f9a053ff453d38129a91df9162
Eztosin <tosinezekiel03@gmail.com>
2024-04-11 19:31:36 +0000

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,27 @@
-import $ from 'jquery';
-import _ from 'lodash';
+function updateCounter() {
+    import $ from 'jquery';
+    import _ from 'lodash';
 
-/*
-* updateCounter - tracks the number of times the button element
-* has been clicked.
-*/
-
-$(document).ready(function () {
-    // Create elements
-    $('<p>Holberton Dashboard</p>').appendTo('body');
-    $('<p>Dashboard data for the students</p>').appendTo('body');
-    $('<button>Click here to get started</button>').appendTo('body').click(_.debounce(updateCounter, 1000));
-    $('<p id="count"></p>').appendTo('body');
-    $('<p>Copyright - Holberton School</p>').appendTo('body');
-
-    let count = 0;
-    function updateCounter() {
-        count++;
+    /*
+    * updateCounter - tracks the number of times the button element
+    * has been clicked.
+    */
+    const updateCounterDebounced = _.debounce(() => {
+        let count = parseInt($('#count').text()) || 0;
+        count += 1;
         $('#count').text(`${count} clicks on the button`);
-    }
-});
+    }, 300);
+
+    $(document).ready(() => {
+        $('<p/>', { text: 'Holberton Dashboard' }).appendTo('body');
+        $('<p/>', { text: 'Dashboard data for the students' })
+            .appendTo('body');
+        $('<button/>', { text: 'Click here to get started' })
+            .appendTo('body').on('click', updateCounterDebounced);
+        $('<p/>', { id: 'count' }).appendTo('body');
+        $('<p/>', { text: 'Copyright - Holberton School' })
+            .appendTo('body');
+    });
+}
+
+updateCounter();
