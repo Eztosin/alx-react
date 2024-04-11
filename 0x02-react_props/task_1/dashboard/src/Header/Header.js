f773e7d0076a835090f2f6e5c99f53d1858c1c2e
Eztosin <tosinezekiel03@gmail.com>
2024-04-11 07:36:21 +0000

    Write the tests for each component

---
+++ 0x02-react_props/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,13 @@
+import logo from './assets/holberton-logo.jpg';
+import 'Header.css';
+
+export default function() {
+    return(
+	    <div className="App">
+	    <div className="App-header">
+	    <img src={logo} className="App-logo" alt="logo" />
+	    <h1>School dashboard</h1>
+	    </div>
+	    </div>
+    );
+}
