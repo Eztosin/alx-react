c95f9469f7784a5bb04a798d64d537bf564f4cb1
Eztosin <tosinezekiel03@gmail.com>
2024-05-07 21:01:12 +0000

    Normalizr & Immutable

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,22 @@
+import { fromJS } from 'immutable';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+import { coursesNormalizer } from '../schema/courses';
+
+const initialState = fromJS({
+  courses: {},
+});
+
+const courseReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_COURSE_SUCCESS:
+      return state.mergeDeep(coursesNormalizer(action.data));
+    case SELECT_COURSE:
+      return state.setIn(['courses', action.index, 'isSelected'], true);
+    case UNSELECT_COURSE:
+      return state.setIn(['courses', action.index, 'isSelected'], false);
+    default:
+      return state;
+  }
+};
+
+export default courseReducer;
---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -0,0 +1,23 @@
+import { fromJS } from 'immutable';
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes';
+import { notificationsNormalizer } from '../schema/notifications';
+
+const initialState = fromJS({
+  filter: 'DEFAULT',
+  notifications: {},
+});
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_NOTIFICATIONS_SUCCESS:
+      return state.mergeDeep(notificationsNormalizer(action.data));
+    case MARK_AS_READ:
+      return state.setIn(['notifications', action.index, 'isRead'], true);
+    case SET_TYPE_FILTER:
+      return state.set('filter', action.filter);
+    default:
+      return state;
+  }
+};
+
+export default notificationReducer;
